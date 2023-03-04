4253a25001c162873384d038313c91eb5b8e40f8
Akshay <jainakshay058@gmail.com>
2023-03-04 15:02:49 +0530

    show movie data

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,26 @@
+import { data } from "../data";
+import React from "react";
+import Navbar from "./Navbar";
+import MovieCard from "./MovieCard";
+
 function App() {
-  return <div className="App">Project Setup</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="main">
+        <div className="tabs">
+          <div className="tab">Movies</div>
+          <div className="tab">Favourites</div>
+        </div>
+
+        <div className="List">
+          {data.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+// import { legacy_createStore } from "redux";
 import "./index.css";
 import App from "./components/App";
+
+// const store = legacy_createStore;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
